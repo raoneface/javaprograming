c4a4c7284d0448e957a0f4c01dd8c6c02d6d501b
Shubham kumar <raoneface@gmail.com>
2026-06-19 12:16:44 +0530

    how real  negatives numbers stores in memory

---
+++ Day_4_Negative_and_floating_Number/floating_negative.java
@@ -0,0 +1,11 @@
+
+public class floating_negative {
+
+    public static void main(String[] args) {
+        float f = 8.125f;
+        System.out.printf("%.20f%n",f );
+        float fnew = .7f;
+
+        System.out.printf("%.20f%n",fnew);
+    }
+}
